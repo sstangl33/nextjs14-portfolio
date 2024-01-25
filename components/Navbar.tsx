1460a6ce8a1d7a29c0f287309942247c2f0c645d
Stephen Stangl <stevestangl@gmail.com>
2024-01-25 11:25:01 -0500

    Add Skills page.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,7 +14,13 @@ const Navbar = () => {
             className="w-full h-full object-contain rounded-full"
           />
         </div>
-        <h1 className="text-white text-[25px] font-semibold">Webchain Dev</h1>
+        <h1 className="text-white text-[25px] font-semibold">
+          Webchain{" "}
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
+            {" "}
+            Dev{" "}
+          </span>
+        </h1>
       </div>
 
       <div className="flex flex-row gap-5 mb-2">
